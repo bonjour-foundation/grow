e388e498aebcb1a83d5dd612bbbdf88e005db19f
David Dal Busco <david.dalbusco@outlook.com>
2026-01-02 09:04:50 +0100

    build: upgrade angular 17 -> 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,17 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 
 import {TranslateModule} from '@ngx-translate/core';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: provideTranslateHttpLoader({prefix: './assets/i18n/', suffix: '.json'}),
-    }),
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
-  ],
-  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        TranslateModule.forRoot({
+            loader: provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
+        }),
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })], providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
