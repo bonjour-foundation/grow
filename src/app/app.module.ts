0fb6a99a7e26e896d962b8a37db8f064412dd48f
David Dal Busco <david.dalbusco@outlook.com>
2026-01-02 09:01:59 +0100

    build: upgrade translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,33 +7,25 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateModule} from '@ngx-translate/core';
+import {HttpClientModule} from '@angular/common/http';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 
-export function exportTranslateStaticLoader(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
-}
-
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        IonicModule.forRoot(),
-        AppRoutingModule,
-        HttpClientModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: exportTranslateStaticLoader,
-                deps: [HttpClient],
-            },
-        }),
-        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    ],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: provideTranslateHttpLoader({prefix: './assets/i18n/', suffix: '.json'}),
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+  ],
+  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
