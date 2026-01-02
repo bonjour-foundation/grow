2fdc16bb489746c884b9538d5b3ff57760b4ae04
David Dal Busco <david.dalbusco@outlook.com>
2026-01-02 10:31:30 +0100

    feat: upgrade from template

--- src/app/guards/goal.guard.ts
+++ src/app/guards/goal.guard.ts
@@ -8,7 +8,7 @@ export class GoalGuard  {
   constructor(private router: Router) {}
 
   async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
-    if (!next || !next.params || !next.params.category || next.params.category === undefined || next.params.category === '') {
+    if (!next || !next.params || !next.params['category'] || next.params['category'] === undefined || next.params['category'] === '') {
       return this.router.parseUrl('/categories');
     } else {
       return true;
