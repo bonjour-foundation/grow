5fd686670f8fc4609542469420b7cbc250ad828f
David Dal Busco <david.dalbusco@outlook.com>
2026-01-02 08:48:55 +0100

    fix: removed import

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,8 +42,6 @@
  *
  */
 
-import './zone-flags.ts';
-
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
