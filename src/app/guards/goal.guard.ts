0a487c505351838a471343fc3d0773f1b12c587a
David Dal Busco <david.dalbusco@outlook.com>
2026-01-02 08:46:39 +0100

    build: upgrade angular 15 -> 16

--- src/app/guards/goal.guard.ts
+++ src/app/guards/goal.guard.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GoalGuard implements CanActivate {
+export class GoalGuard  {
   constructor(private router: Router) {}
 
   async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
